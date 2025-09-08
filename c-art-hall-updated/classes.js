348fcb31483fc245202cda5dc6e23bd3f2429cc9
gleversonrafael <gleverson_rafael@hotmail.com>
2025-09-08 11:58:42 -0300

    started modal creation in art hall

---
+++ c-art-hall-updated/classes.js
@@ -0,0 +1,49 @@
+class Arts {
+    constructor() {
+        this.artsMap = new Map();
+    }
+
+    pushArt(identifier, art) {
+        this.artsMap.set(identifier, art);
+    }
+    
+    loadArtsInConsole() {
+        console.log(this.artsMap);
+    }
+
+    loadArtsInElement(html) {
+        for(let art of this.artsMap) {
+            html.innerHTML += `
+            <div class="item">
+                <div>
+                    <img class="item-image" src="${art[1].imageSource}">
+                    <div class="icon"></div>
+
+                </div>
+
+                <div class="item-details"> 
+                    <p class="major-title"> ${art[1].name} </p>
+                    <p> ${art[1].creator} </p>
+                    <p> ${art[1].type} </p>
+
+                    <article> ${art[1].description} </article>
+                
+                </div>
+
+            </div>`;
+        }
+    }
+}
+
+class Art {
+    constructor(identifier, artName, artCreator, artImage, artType, artDescription) {
+        this.identifier = identifier;
+
+        this.name = artName;
+        this.creator = artCreator;
+        this.imageSource = artImage; // FOR DESKTOP IMAGES??
+        
+        this.type = artType;
+        this.description = artDescription;
+    }
+}
